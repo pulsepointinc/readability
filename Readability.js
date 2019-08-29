c3a8bb15b4b25a28cb05001be130448aa8891102
Joe Winett <joe@winett.com>
2019-08-29 02:33:28 -0500

    Remove aria-hidden="true" nodes (fixes #541) (#555)
    Remove aria-hidden="true" nodes (fixes #541)

--- Readability.js
+++ Readability.js
@@ -1794,7 +1794,9 @@ Readability.prototype = {
   },
 
   _isProbablyVisible: function(node) {
-    return (!node.style || node.style.display != "none") && !node.hasAttribute("hidden");
+    return (!node.style || node.style.display != "none")
+      && !node.hasAttribute("hidden")
+      && (!node.hasAttribute("aria-hidden") || node.getAttribute("aria-hidden") != "true");
   },
 
   /**
